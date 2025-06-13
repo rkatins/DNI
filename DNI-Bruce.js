0f59f7d607f6fe4c3a2291660d11c3b3131a04dd
Rubén Katins Durán <katinsruben.98@gmail.com>
2025-06-13 21:44:21 +0200

    Versión definitiva
    Obtiene la letra correctamente, el while se ejecuta en bucle hasta que se se pulsa algun botón y a continuación muestra el teclado para introducir el número

--- DNI-Bruce.js
+++ DNI-Bruce.js
@@ -4,26 +4,25 @@ const keyboard = require('keyboard')
 
 var letrasPosibles = 'TRWAGMYFPDXBNJZSQVHLCKE'
 
-println('Ingrese el numero DNI (Sin letra):')
+while (!keyboard.getAnyPress()) {
+    println('Ingrese el numero DNI \n(Sin letra):\n\nPulsa una tecla para continuar')
+}
 
-if (keyboard.getAnyPress()) {
-    var numeroIntroducido_STRING = dialog.prompt()
-    var numeroIntroducido = parse_int(numeroIntroducido_STRING)
+var numeroIntroducido_STRING = dialog.prompt()
+var numeroIntroducido = parse_int(numeroIntroducido_STRING)
 
-    var moduloLetra = numeroIntroducido % 23;
-    var letraDNI = letrasPosibles.charAt(moduloLetra);
+var moduloLetra = numeroIntroducido % 23;
+var letraDNI = letrasPosibles.charAt(moduloLetra);
 
-    if (numeroIntroducido_STRING === null || numeroIntroducido_STRING === undefined) {
-        println('numeroIntroducido_STRING -> nul')
-    } else if (numeroIntroducido_STRING === '') {
-        println('numeroIntroducido_STRING -> Vacio')
-    } else if (isNaN(numeroIntroducido)) {
-        println('numeroIntroducido -> NaN')
-    } else if (isNaN(moduloLetra)) {
-        println('moduloLetra -> NaN')
-    } else {
-        println('Letra -> ' + letraDNI)
-    }
-
+if (numeroIntroducido_STRING === null || numeroIntroducido_STRING === undefined) {
+    println('numeroIntroducido_STRING -> nul')
+} else if (numeroIntroducido_STRING === '') {
+    println('numeroIntroducido_STRING -> Vacio')
+} else if (isNaN(numeroIntroducido)) {
+    println('numeroIntroducido -> NaN')
+} else if (isNaN(moduloLetra)) {
+    println('moduloLetra -> NaN')
+} else {
+    println('Letra -> ' + letraDNI)
     delay(8000)
 }
