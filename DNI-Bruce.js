18347c6c9b9bf9e08902e399dbfdd1287d6253df
Rubén Katins Durán <katinsruben.98@gmail.com>
2025-06-13 21:25:45 +0200

    Se va a probar a detectar que se a presionado uno de los botones a modo de "validación" y poder continuar

--- DNI-Bruce.js
+++ DNI-Bruce.js
@@ -1,25 +1,28 @@
 // Importaciones
-const dialog = require('dialog');
+const dialog = require('dialog')
+const keyboard = require('keyboard')
 
 var letrasPosibles = 'TRWAGMYFPDXBNJZSQVHLCKE'
 
-var numeroIntroducido_STRING = dialog.prompt('Ingrese el numero DNI (Sin letra):')
-println('"' + numeroIntroducido_STRING + '"')
-var numeroIntroducido = parse_int(numeroIntroducido_STRING)
+println('Ingrese el numero DNI (Sin letra):')
+if (keyboard.getNextPress()) {
+    var numeroIntroducido_STRING = dialog.prompt()
+    var numeroIntroducido = parse_int(numeroIntroducido_STRING)
 
-var moduloLetra = numeroIntroducido % 23;
-var letraDNI = letrasPosibles.charAt(moduloLetra);
+    var moduloLetra = numeroIntroducido % 23;
+    var letraDNI = letrasPosibles.charAt(moduloLetra);
 
-if (numeroIntroducido_STRING === null || numeroIntroducido_STRING === undefined) {
-    println('numeroIntroducido_STRING -> nul')
-} else if (numeroIntroducido_STRING === '') {
-    println('numeroIntroducido_STRING -> Vacio')
-} else if (isNaN(numeroIntroducido)) {
-	println('numeroIntroducido -> NaN')
-} else if (isNaN(moduloLetra)) {
-	println('moduloLetra -> NaN')
-} else {
-	println('Letra -> ' + letraDNI)
+    if (numeroIntroducido_STRING === null || numeroIntroducido_STRING === undefined) {
+        println('numeroIntroducido_STRING -> nul')
+    } else if (numeroIntroducido_STRING === '') {
+        println('numeroIntroducido_STRING -> Vacio')
+    } else if (isNaN(numeroIntroducido)) {
+        println('numeroIntroducido -> NaN')
+    } else if (isNaN(moduloLetra)) {
+        println('moduloLetra -> NaN')
+    } else {
+        println('Letra -> ' + letraDNI)
+    }
+
+    delay(8000)
 }
-
-delay(8000)
