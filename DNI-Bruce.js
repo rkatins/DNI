60eda3f8541459b5b1d6c595c7e8149694f1b5aa
Rubén Katins Durán <katinsruben.98@gmail.com>
2025-06-13 20:42:49 +0200

    Sabemos que `numeroIntroducido_STRING` no puede ser porque recibe la entrada bien

--- DNI-Bruce.js
+++ DNI-Bruce.js
@@ -4,11 +4,7 @@ const dialog = require('dialog');
 var letrasPosibles = 'TRWAGMYFPDXBNJZSQVHLCKE'
 
 var numeroIntroducido_STRING = dialog.prompt('Ingrese el numero DNI (Sin letra):')
-
-println('Entrada (STRING): ' + numeroIntroducido_STRING)
-
 var numeroIntroducido = parse_int(numeroIntroducido_STRING)
-println('Entrada convertido: ' + numeroIntroducido)
 
 var moduloLetra = numeroIntroducido % 23;
 var letraDNI = letrasPosibles.charAt(moduloLetra);
