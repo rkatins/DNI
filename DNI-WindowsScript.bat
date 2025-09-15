ed3027a2f3016fb244391d6ed597f6ccfa3d31b0
Rubén Katins Durán <83010981+rkatins@users.noreply.github.com>
2025-09-16 00:08:06 +0200

    Version final actualizada

--- DNI-WindowsScript.bat
+++ DNI-WindowsScript.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 SETLOCAL EnableDelayedExpansion
 
-@REM Cuando se declara una variable que guarde una cadena de texto, las comillas dobles que se suelen usar para delimitar la cadena de texto añaden "un caracter más" a la cadena de texto que hemos escrito, por lo que es recomendable incluir entre las comillas a la propia variable
+@REM *1
 SET "letra=TRWAGMYFPDXBNJZSQVHLCKE"
 
 ECHO Ingrese el numero de DNI sin letra: 
@@ -16,6 +16,6 @@ ECHO.
 ECHO letra: !caracter_extraido!
 PAUSE
 
-@REM Consejo antes de salir, evitar poner espacios antes/depues de la declaracion del nombre de una variable porque puede romper los calculos aritmeticos del script o similares
+@REM *2
 EXIT
 CLS
